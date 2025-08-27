c0dc37a98b78830a821b4a7ab34b95f683dba83a
Rofhiwa Muvhulawa <rofhiwamuvhulawa13@gmail.com>
2025-08-27 13:29:05 +0200

    Add files via upload

---
+++ crm/settings.py
@@ -0,0 +1,22 @@
+# ... existing code ...
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+    'django_filters',
+    'django_crontab',  # Add this line
+    'crm',
+]
+
+# ... existing code ...
+
+# Add at the end of the file
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),  # Every 12 hours
+]
